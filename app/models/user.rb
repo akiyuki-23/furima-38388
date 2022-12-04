ecee5caf6ae18c8f44d991fb6cbbfc4c2bbd37d3
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2022-12-04 10:27:19 +0900

    パスワードの半角英数字のみ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,7 @@ class User < ApplicationRecord
       validates :first_name_kana
     end
   end
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password, format:{ with: VALID_PASSWORD_REGEX}
 end
