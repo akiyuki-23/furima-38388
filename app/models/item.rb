3a987fd220ed0f5ee70350b51e06495d94804d4b
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2022-12-07 14:26:07 +0900

    ActiveHashの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Association::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :item_scheduled_delivery
+
+  with_options presence:true do
+    validates :item_name
+    validates :item_info
+    validates :item_price, format: { with: /\A[0-9]+\z/ }, numericality: { in: 300..9,999,999, 'is invalid' }
+  end
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :item_scheduled_delivery_id
+  end
 end
