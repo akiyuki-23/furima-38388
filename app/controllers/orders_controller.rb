f09fced7a82efbdd238edd7be082bd690a1fe1e7
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2022-12-20 00:26:53 +0900

    FormObject作成後のVIEWの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,23 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.new
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
   private
+
   def order_params
-    params.require(:order).merge(item_id: current_item.id, user_id: current_user.id)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
   end
 end
