776aecdab418618425179fbda131747ed631b165
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2022-12-19 14:39:25 +0900

    order controllerの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,20 @@
+class OrdersController < ApplicationController
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).merge(item_id: current_item.id, user_id: current_user.id)
+  end
+end
