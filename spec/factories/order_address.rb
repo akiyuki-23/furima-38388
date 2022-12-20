f3dbc1d69bf6b7f7e334ece9725a94fdaee2dd08
akiyuki-23 <113977720+akiyuki-23@users.noreply.github.com>
2022-12-20 16:11:08 +0900

    テストコードの作成

---
+++ spec/factories/order_address.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code { '123-4567' }
+    prefecture_id { '2' }
+    city { '千歳市' }
+    address { '知曲2-5' }
+    building { 'コーポ千歳' }
+    phone_number { '04098764548' }
+  end
+end
